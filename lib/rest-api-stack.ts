bec4ffd3a3d325bfe20ac3907086d710c74d0bd9
Tibor Molnar <tmolnair@gmail.com>
2025-03-01 15:38:36 +0000

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -37,8 +37,19 @@ export class RestAPIStack extends cdk.Stack {
           TABLE_NAME: moviesTable.tableName,
           REGION: 'eu-west-1',
         },
-      }
-      );
+      });
+
+      const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_22_X,
+        entry: `${__dirname}/../lambdas/addMovie.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: "eu-west-1",
+        },
+      });
       
       const getAllMoviesFn = new lambdanode.NodejsFunction(
         this,
@@ -75,6 +86,8 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn) // add function
+
         
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
@@ -95,6 +108,11 @@ export class RestAPIStack extends cdk.Stack {
     moviesEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
+    );
+    // NEW
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
     );
     // Detail movie endpoint
     const specificMovieEndpoint = moviesEndpoint.addResource("{movieId}");
